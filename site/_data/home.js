1317f1b9ddec29380eac6c04eb192306f67bf041
Shishak <semenshishak@gmail.com>
2020-01-31 07:57:18 +0200

    Update home.js

--- site/_data/home.js
+++ site/_data/home.js
@@ -1,7 +1,7 @@
 var axios  = require('axios');
 var toJSON = require('xml2js').parseString;
 
-var url = process.env.MEDIUM_FEED ||'https://24tv.ua/rss/tag/1792.xml';
+var url = process.env.MEDIUM_FEED ||'https://beautifuleuropeangirls.tumblr.com/rss';
 
 module.exports = () => {
   return new Promise((resolve, reject) => {
