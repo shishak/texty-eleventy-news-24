226da3e0df21b3b381d5ebbdd5b4a9c45fa3e485
Shishak <semenshishak@gmail.com>
2020-01-31 07:59:44 +0200

    Update home.js

--- site/_data/home.js
+++ site/_data/home.js
@@ -1,7 +1,7 @@
 var axios  = require('axios');
 var toJSON = require('xml2js').parseString;
 
-var url = process.env.MEDIUM_FEED ||'https://beautifuleuropeangirls.tumblr.com/rss';
+var url = process.env.MEDIUM_FEED ||'https://www.jpl.nasa.gov/multimedia/rss/news.xml';
 
 module.exports = () => {
   return new Promise((resolve, reject) => {
